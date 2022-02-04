2235e188c8d78e0ac91d45f2dd681b28b2e1f319
Odiljon Gaybullaev <odiljongaybullayev@mail.ru>
2022-02-04 10:50:22 +0500

    work

--- website/auth.py
+++ website/auth.py
@@ -1,33 +1,37 @@
-from flask import Blueprint, render_template, request, flash,redirect,url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
-from werkzeug.security import generate_password_hash,check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required,logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-   if request.method=='POST':
-       email=request.form.get('email')
-       password=request.form.get('password')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-       user=User.query.filter_by(email=email).first()
-       if user:
-           if check_password_hash(user.password,password):
-               flash('Tizimga kirdingiz', category='success')
-               return redirect(url_for('views.home'))
-           else:
-               flash('Parolingiz noto`g`ri, yana urinib ko`ring',category='error')
-       else:
-           flash('Pochta mavjud emas', category='error')
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Tizimga kirdingiz', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Parolingiz noto`g`ri, yana urinib ko`ring', category='error')
+        else:
+            flash('Pochta mavjud emas', category='error')
 
-   return render_template("login.html", text="Test", user="C0mplex")
+    return render_template("login.html",user=current_user)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('sign-up', methods=['GET', 'POST'])
@@ -47,9 +51,11 @@ def sign_up():
         elif len(password1) < 7:
             flash('Parol 7 ta belgidan ko`p bo`lishi kerak.', category='error')
         else:
-            new_user=User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            new_user = User(email=email, first_name=first_name,
+                            password=generate_password_hash(password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Ro`yxatdan o`tdingiz', category='success')
             return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+    return render_template("sign_up.html",user=current_user)
